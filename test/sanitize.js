22ac0bb3531835a3a4357db9a95aba96d0d9ebcf
Ben Fonarov <ben.fonarov@gmail.com>
2014-04-02 13:44:23 -0400

    changed a bit of the sanitization functionality to enable param checks

--- test/sanitize.js
+++ test/sanitize.js
@@ -1,20 +1,34 @@
-var assert = require('assert'),
-isCypher = require('../lib/helpers/isCypher');
+var assert = require('assert');
 
 
 describe('isCypher should return True or False if the string contains any cypher injection', function () {
 
-    it(' Should say True to url that is cypher injection free', function () {
-        string = 'This is a test';
-        o = isCypher(string);
-        assert.equal(o, false);
-
-        });
-
-    it('Should catch Cypher Keywords', function () {
-
-        string = '/app_model/12_MATCH/12/';
-        o = isCypher(string);
+    it('Should return True to params object that is cypher injection free', function () {
+        var adapter = require('../lib/adapter.js');
+        params = ['This is a test'];
+        o = adapter.sanitized(params);
         assert.equal(o, true);
-    })
+
+    });
+
+    it('Should return False for params object with Cypher Keywords', function () {
+        var adapter = require('../lib/adapter.js');
+        params = ['/app_model/12_MATCH/12/'];
+        o = adapter.sanitized(params);
+        assert.equal(o, false);
+    });
+
+    it('Should return False for params object with multiple params and Cypher Keywords', function () {
+        var adapter = require('../lib/adapter.js');
+        params = ['/app_model/12_MATCH/12/', 'something', 'test', 'START n=node(1)'];
+        o = adapter.sanitized(params);
+        assert.equal(o, false);
+    });
+
+    it('Should return True for params object with multiple params and without Cypher Keywords', function () {
+        var adapter = require('../lib/adapter.js');
+        params = ['/app_model/', 'something', 'test', 'something2'];
+        o = adapter.sanitized(params);
+        assert.equal(o, true);
+    });
 });
