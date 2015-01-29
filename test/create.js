6ffbfd23da582797623c0be1d055dfb9e1eb50f5
dughetti <dughetti@zesmind.com>
2015-01-29 10:16:39 -0300

    Upgrade adapter for sails 0.10.5. Initial version

--- test/create.js
+++ test/create.js
@@ -5,7 +5,7 @@ describe('Creating Nodes', function () {
 	it('should create one node with a property test = "1"', function (done) {
 		var adapter = require('../lib/adapter.js');
 		adapter.createConnection();
-		adapter.create(null, { test: 1 }, function(err, results) {
+		adapter.create(null, null, { test: 1 }, function(err, results) {
 			if (err) { throw err; }
 			nodes.push(results);
 			done();
@@ -15,7 +15,7 @@ describe('Creating Nodes', function () {
 	it('should create multiple nodes with the property test = "1"', function(done) {
 		var adapter = require('../lib/adapter.js');
 		adapter.createConnection();
-		adapter.createMany(null,{params:[{ test: 1 },{ test: 1 }]}, function(err, results) {
+		adapter.createMany(null, null,{params:[{ test: 1 },{ test: 1 }]}, function(err, results) {
 			if (err) { throw err; }
 			nodes.push(results);
 			done();
--- test/sanitize.js
+++ test/sanitize.js
@@ -6,7 +6,7 @@ describe('isCypher should return True or False if the string contains any cypher
     it('Should return True to params object that is cypher injection free', function () {
         var adapter = require('../lib/adapter.js');
         params = {i: 'This is a test' };
-        o = adapter.sanitized(params);
+        o = adapter.sanitized(null, null, params);
         assert.equal(o, true);
 
     });
@@ -14,21 +14,21 @@ describe('isCypher should return True or False if the string contains any cypher
     it('Should return False for params object with Cypher Keywords', function () {
         var adapter = require('../lib/adapter.js');
         params = {i: '/app_model/12_MATCH/12/' };
-        o = adapter.sanitized(params);
+        o = adapter.sanitized(null, null, params);
         assert.equal(o, false);
     });
 
     it('Should return False for params object with multiple params and Cypher Keywords', function () {
         var adapter = require('../lib/adapter.js');
         params = {i: '/app_model/COUNT/12/', x: 'something', t: { z: 'test', y: 'START n=node(1)'} };
-        o = adapter.sanitized(params);
+        o = adapter.sanitized(null, null, params);
         assert.equal(o, false);
     });
 
     it('Should return True for params object with multiple params and without Cypher Keywords', function () {
         var adapter = require('../lib/adapter.js');
         params = {i: '/app_model/', x: 'something', t: { z: 'test', y: 'something2'} };
-        o = adapter.sanitized(params);
+        o = adapter.sanitized(null, null, params);
         assert.equal(o, true);
     });
 });
